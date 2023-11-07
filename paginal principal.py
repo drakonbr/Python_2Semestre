15ac2372b09633c96d70d12cbd5d8500e153655c
drakonbr <45700302+drakonbr@users.noreply.github.com>
2023-11-07 17:01:37 -0300

    Update paginal principal.py
    att- 1

--- paginal principal.py
+++ paginal principal.py
@@ -1,7 +1,52 @@
-while True:
-    print(f"\033[34m{separador}\033[m\n")
-    inicar = input("{:^34}".format("ENTER PARA INICIAR"))
-    if inicar != "9999":
-        main()
+import os
+os.system('cls')
+import os
+os.system('cls')
+enter = "ckas"
+## Colocar o inicar.
+
+def adicionando():
+    livro = input("Coloque o nome do livro: ")
+    #livro.append(arquivo(txt))
+    autor = input("Coloque o nome do autor: ")
+    #autor.append(arquivo(txt))
+    valor = float(input("Coloque o valor do livro: "))
+    #valor.append(aquivo(txt))
+    
+
+def inicio():
+    print ("//////Escolha o caminho//////")
+    print ("1- Livros que Possui\n2- Lista de desejos\n3- Checar gastos")
+    escolha = int(input("-"))
+
+    if escolha == 1:
+        #print(Lista_livros)    #manipulação de arquivos
+        print("1- Adicionar \n2-remover")
+        escolha2 = int(input("-"))
+        if escolha2 == 1:
+            adicionando()
+        elif escolha2 == 2:
+            removendo()
+        
+        
+        
+
+    elif escolha == 2:
+        print()
+
+    elif escolha ==3:
+        print()
+
     else:
-        print("\033[31mOpção inválida.\033[m")
+        print("Valor invalido")
+        cetras = input("Prescine enter")
+        if cetras != enter:
+            inicio()
+inicio()
+
+def removendo():
+    print("Qual produto você deseja remover: ")
+    escolha_remover = int(input("-"))
+    if escolha_remover == 1:
+        print("nada ainda")
+
