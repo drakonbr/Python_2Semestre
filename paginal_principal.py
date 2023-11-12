bd6091d2a961d0ff92344a63e7ab211eabe97672
Gabriel Q S Landim <142419701+Gabrielqlandim@users.noreply.github.com>
2023-11-12 14:29:18 -0300

    Update paginal_principal.py
    além da estilização do codigo, eu mexi em algumas partes que tinham erro de ortografia e também testei o código

--- paginal_principal.py
+++ paginal_principal.py
@@ -6,22 +6,22 @@ enter = "ckas"
 ## Colocar o inicar.
 
 def adicionando():
-    livro = input("Coloque o nome do livro: ")
+    livro = input("\033[1;34;49mColoque o nome do livro:\033[m ")
     #livro.append(arquivo(txt))
-    autor = input("Coloque o nome do autor: ")
+    autor = input("\033[1;32;49mColoque o nome do autor:\033[m ")
     #autor.append(arquivo(txt))
-    valor = float(input("Coloque o valor do livro: "))
+    valor = float(input("\033[1;33;49mColoque o valor do livro:\033[m "))
     #valor.append(aquivo(txt))
     
 
 def inicio():
-    print ("//////Escolha o caminho//////")
-    print ("1- Livros que Possui\n2- Lista de desejos\n3- Checar gastos")
+    print ("\033[1;32;49m//////Escolha o caminho//////\033[m")
+    print ("1- \033[0;33;49mLivros que você Possui\n\033[m2- \033[0;35;49mSua lista de desejos\n\033[m3- \033[0;31;49mChecar seus gastos\033[m")
     escolha = int(input("-"))
 
     if escolha == 1:
         #print(Lista_livros)    #manipulação de arquivos
-        print("1- Adicionar \n2-remover")
+        print("1- \033[0;36;49mAdicionar Livro\n\033[m2- \033[0;31;49mRemover Livro\033[m")
         escolha2 = int(input("-"))
         if escolha2 == 1:
             adicionando()
@@ -38,15 +38,14 @@ def inicio():
         print()
 
     else:
-        print("Valor invalido")
-        cetras = input("Prescine enter")
+        print("\033[0;33;41mValor inválido\033[m")
+        cetras = input("\033[1;36;49mPressione ENTER\033[m")
         if cetras != enter:
             inicio()
 inicio()
 
 def removendo():
-    print("Qual produto você deseja remover: ")
+    print("\033[1;31;43mQual produto você deseja remover:\033[m ")
     escolha_remover = int(input("-"))
     if escolha_remover == 1:
-        print("nada ainda")
-#
+        print("\033[4;36;49mNada ainda\033[m")
