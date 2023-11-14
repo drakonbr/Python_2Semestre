5e635dd357e6770c54b77aa32266583d5d234fcd
LukasF <lfta@cesar.school>
2023-11-14 09:42:05 -0300

    codigo atualizado pesadamente, falta apenas as funções de extrato e função extra

--- paginal_principal.py
+++ paginal_principal.py
@@ -2,29 +2,79 @@ import os
 os.system('cls')
 enter = "ckas"
 
+path = "livro_lista.txt"
 
 def visualizar():
-    #autor.append(arquivo(txt)
-    return
+    op = 0
+    f = open(path,'r', encoding='utf-8')
+    opr = input('Coloque o gênero que você deseja filtar: ').strip().lower()
+    for l in f:
+        nome = (l.split(';'))[0]
+        autor = (l.split(';'))[1]
+        genero = (l.split(';'))[2]
+        valor = (l.split(';'))[3]
+        if opr == genero:
+            print(f'Nome: {nome.title()}, Autor: {autor.title()}, Gênero: {genero.title()} Preço: {valor}')
+            op += 1
+    f.close()
+
+    if op == 0:
+        return "Não há livros nessa categoria"
+    else:
+        return ""
 
 def adicionar():
-    livro = input("\033[1;34;49mColoque o nome do livro:\033[m ")
-    #livro.append(arquivo(txt))
-    autor = input("\033[1;32;49mColoque o nome do autor:\033[m ")
-    #autor.append(arquivo(txt))
-    valor = float(input("\033[1;33;49mColoque o valor do livro:\033[m "))
-    #valor.append(arquivo(txt))
+    f = open(path, 'a', encoding='utf-8')
+    livro = input("\033[1;34;49mColoque o nome do livro: \033[m ").strip().lower()
+    autor = input("\033[1;32;49mColoque o nome do autor: \033[m ").strip().lower()
+    genero = input("Coloque o gênero do livro: ").strip().lower()
+    valor = float(input("\033[1;33;49mColoque o valor do livro: \033[m "))
+    f.write(f"{livro};{autor};{genero};{valor}\n")
+    f.close()
     return "Livro Adicionado!"
 
 def remover():
-    print("\033[1;31;43mQual livro você deseja remover:\033[m ")
-    return "Livro Removido!"
+    op = 0
+    f = open(path,'r')
+    fr = f.readlines()
+    f.close()
+
+    f = open(path, 'w', encoding='utf-8')
+    removed = input("\033[1;31;43mQual livro você deseja remover: \033[m ").strip().lower()
+    for l in fr:
+        if removed != (l.split(';'))[0]:
+            f.write(f'{l}\n')
+        else:
+            op += 1
+    f.close()
+    if op >= 1:
+        return "Livro Removido!"   
+    else:
+        return "Esse livro não está na biblioteca" 
 
 def atualizar():
-    autor = input("\033[1;32;49mColoque o nome do autor:\033[m ")
-    valor = float(input("\033[1;33;49mColoque o valor do livro:\033[m "))
-    return "Livro Atualizado"
+    op = 0
+    f = open(path,'r')
+    fr = f.readlines()
+    f.close()
 
+    f = open(path, 'w', encoding='utf-8')
+    nome = input("\033[1;34;49mColoque o nome do livro que você deseja atualizar: \033[m ").strip().lower()
+    for l in fr:
+        if nome == (l.split(';'))[0]:
+            autor = input("\033[1;32;49mColoque o nome do autor: \033[m ").strip().lower()
+            genero = input("Coloque o gênero do livro: ").strip().lower()
+            valor = float(input("\033[1;33;49mColoque o valor do livro: \033[m "))
+            f.write(f'{nome};{autor};{genero};{valor}')
+            op += 1
+        else:
+            f.write(f'{l}\n')
+    f.close()
+    if op >= 1:
+        return "Livro Atualizado!"
+    else:
+        return "Esse livro não está na biblioteca" 
+    
 def desejar():
     return
 def extrato():
@@ -32,27 +82,25 @@ def extrato():
 
 def inicio():
     print ("\033[1;32;49m//////Escolha o caminho//////\033[m")
-    print ("1- \033[0;33;49mVisualizar Livros\n\033[m2- \033[0;36;49mAdicionar Livro\n\033[m3- \033[0;31;49mRemover Livro\033[m 4-Atualizar Livro\n 5- Lista de Desejos\n 6- Extrato")
-    escolha = int(input("-"))
-
+    print ("1- \033[0;33;49mVisualizar Livros\n\033[m2- \033[0;36;49mAdicionar Livro\n\033[m3- \033[0;31;49mRemover Livro\n\033[m 4- Atualizar Livro\n 5- Lista de Desejos\n 6- Extrato")
+    escolha = int(input("> "))
     if escolha == 1:
-        visualizar()
+        print(visualizar())
     elif escolha == 2:
-        adicionar()
+        print(adicionar())
     elif escolha ==3:
-        remover()
+        print(remover())
     elif escolha == 4:
-        atualizar()
+        print(atualizar())
     elif escolha == 5:
-        desejar()
+        print(desejar())
     elif escolha == 6:
-        extrato()
+        print(extrato())
     else:
         print("\033[0;33;41mValor inválido\033[m")
         cetras = input("\033[1;36;49mPressione ENTER\033[m")
         if cetras != enter:
             inicio()
 
-
-
-inicio()
+while True:
+    inicio()
